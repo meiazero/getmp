b5d7b4b84ac9c8d417819c668f2358cb850f93ec
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-05-23 16:16:30 -0300

    refactor: get metrics individual

--- src/get_all.py
+++ src/get_all.py
@@ -19,29 +19,33 @@ URLS = [CPU, MEMORIA, FSREADS, FSWRITES,
         PACKETSREC, PACKETSTRANS, BYTESREC, BYTESTRANS]
 
 
-def GetMetrics():
-    metrics = []
-    for url in URLS:
-        res: str = request.urlopen(url).read()
-        json_dict: dict = json.loads(res.decode('utf-8'))
-        out_dict: dict = [x for x in json_dict['data']
-                          ['result'][0]['value']]
-        value_zero = str(out_dict[0])
-        metrics.append(value_zero)
-
-    WriteMetricsOnFile(metrics)
+def ReqMetrics(url, output, type='metric'):
+    res: str = request.urlopen(url).read()
+    json_dict: dict = json.loads(res.decode('utf-8'))
+    out_dict: dict = [x for x in json_dict['data']
+                      ['result'][0]['value']]
+    value_zero = str(out_dict[1])
+    WriteMetricsOnFile(value=value_zero, locale=output, metric_name=type)
 
 
-def WriteMetricsOnFile(value, locale='.'):
+def WriteMetricsOnFile(value, locale='.', metric_name="metric",):
     if locale:
         os.makedirs(f'{locale}/metrics', exist_ok=True)
 
-        with open(f'{locale}/metrics/{date.today()}.txt', "+a") as file:
-            file.write(
-                f'cpu,memoria,fsreads,fswrites,packetsrec,packetstrans,bytesrec,bytestrans\n')
+        with open(f'{locale}/metrics/{metric_name}-{date.today()}.csv', "+a") as file:
+            # file.write(f'cpu,memoria,fsreads,fswrites,packetsrec,packetstrans,bytesrec,bytestrans\n')
             # after test check if the loop is here.
-            file.write(f'{value},\n')
+            file.write(f'{value}\n')
 
 
 if __name__ == "__main__":
-    GetMetrics()
+    while True:
+        ReqMetrics(CPU, '.', 'cpu')
+        ReqMetrics(MEMORIA, '.', 'memoria')
+        ReqMetrics(FSREADS, '.', 'fsreads')
+        ReqMetrics(FSWRITES, '.', 'fswrites')
+        ReqMetrics(PACKETSREC, '.', 'pkgrec')
+        ReqMetrics(PACKETSTRANS, '.', 'pkgtrans')
+        ReqMetrics(BYTESREC, '.', 'byrec')
+        ReqMetrics(BYTESTRANS,  '.', 'bytrans')
+        time.sleep(5)
