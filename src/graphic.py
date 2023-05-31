35d95c316f9facf38aeec3ae55967ef5025664f0
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-05-31 10:21:09 -0300

    update

--- src/graphic.py
+++ src/graphic.py
@@ -1,33 +1,22 @@
-# plot the graph from the data in the text file
 import matplotlib.pyplot as plt
-
-# Read data from the text file
-
-
-def CalculateDiferenceValue(value_1, value_2):
-    return (value_2 - value_1)
 
 
 def read_data(file_name):
     x = []
     y = []
-    timeInSeconds = 0
-    tmp = 0
-    with open(file_name, 'r') as file:
-        for line in file:
-            values = line.split()
 
-            timeInSeconds += 5
-            tmp = values[0]
-            try:
-                y.append(CalculateDiferenceValue(float(tmp), float(values[1])))
-            except:
-                y.append(float(tmp))
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                values = line.split()
+                x_value = float(values[0])
+                y_value = float(values[1])
+                x.append(x_value)
+                y.append(y_value)
+    except FileNotFoundError:
+        print("Arquivo não encontrado:", file_name)
 
-            x.append(float(timeInSeconds))
     return x, y
-
-# Plot the graph
 
 
 def plot_graph(x, y, x_label='', y_label='', title='titulo'):
@@ -36,19 +25,20 @@ def plot_graph(x, y, x_label='', y_label='', title='titulo'):
     plt.ylabel(y_label)
     plt.title(title)
     plt.grid(True)
+    # plt.savefig(output)
     plt.show()
-
-# Main function
 
 
 def main(path, label_x='', label_y='', title='', output='.'):
-    # file_name = './metrics/memoria-2023-05-24.csv'  # Update with your file name
     file_name = path
     x, y = read_data(file_name)
     plot_graph(x, y, x_label=label_x, y_label=label_y, title=title)
-    # plt.savefig(output)
 
 
 if __name__ == '__main__':
-    main(path="./metrics/pkgtrans-2023-05-24.csv",
-         label_x="tempo (s)", label_y="packets", title="PACKETS TRANSMITTED (%)")
+    main(
+        path="./metrics/pkgtrans-2023-05-24.csv",
+        label_x="tempo (s)",
+        label_y="packets",
+        title="PACKETS TRANSMITTED (%)"
+    )
